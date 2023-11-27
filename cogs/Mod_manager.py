34c695ef2c2c3bdb44026decc01845e70720e958
Tomas-Ka <Tomas.kamsater@gmail.com>
2023-11-27 02:51:28 +0100

    Documentation and embeds Updated documentation to Mod_manager, and also added some more fancy embeds.

--- cogs/Mod_manager.py
+++ cogs/Mod_manager.py
@@ -5,22 +5,29 @@ import discord
 from db_handler import DBHandler
 from helpers import Action, Moderator, Guild, VacationWeek
 import time
-# TODO; WRITE DOCUMENTATION
-# I'm just too tired rn :P
 
+# ? global colour for the bot. Change this when we get around to a cohesive theme and whatnot.
+global colour
+colour = 0x1dff1a
 
 class ConfigView(discord.ui.View):
+    """View for the config message."""
     def __init__(self, db: DBHandler) -> None:
         super().__init__()
         self.mod_category_id = 0
         self.mod_category_name = "Null"
         self.roles = []
+        self.wait_time = None
         self.db = db
 
     @discord.ui.select(cls=discord.ui.ChannelSelect,
                        channel_types=[discord.ChannelType.category],
                        placeholder="Select moderator category")
     async def channel_select(self, interaction: discord.Interaction, select: discord.ui.ChannelSelect) -> None:
+        # Lets the user select a channel category from a dropdown.
+        # This function is run every time they change the dropdown.
+        # The channel select chooses what category we don't track messages in
+        # when we initialize the bot for a new server/guild.
         self.mod_category_id = select.values[0].id
         self.mod_category_name = select.values[0].name
         await interaction.response.defer()
@@ -30,39 +37,73 @@ class ConfigView(discord.ui.View):
                        min_values=0,
                        max_values=25)
     async def role_select(self, interaction: discord.Interaction, select: discord.ui.RoleSelect) -> None:
+        # Lets the user select one or multiple roles from a dropdown.
+        # This function is run every time they change the dropdown.
+        # The role select chooses who we add automatically as moderators
+        # when we initialize the bot for a new server/guild
         self.roles = select.values
+        await interaction.response.defer()
+    
+    @discord.ui.select(cls=discord.ui.Select,
+                       options=[
+                           discord.SelectOption(label="1 week", description="check moderator stats once every week", value="7"),
+                           discord.SelectOption(label="2 weeks", description="check moderator stats once every other week", value="14"),
+                           discord.SelectOption(label="4 weeks", description="check moderator stats once every 4 weeks", value="28")
+                       ], placeholder="Select how much time we should wait between moderator checks")
+    async def wait_time_select(self, interaction: discord.Interaction, select: discord.ui.Select) -> None:
+        # Lets the user select one of the predetermined valeus from the dropdown.
+        # This setting can also be changed later with a slash command
+        self.wait_time = select.values[0]
         await interaction.response.defer()
 
     @discord.ui.button(style=discord.ButtonStyle.success, label="Confirm")
     async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        # Make sure all our fields have proper values and error if not
         if (self.mod_category_id == 0):
             await interaction.response.send_message("You have to select a category!", ephemeral=True)
             return
+        if not self.wait_time:
+            await interaction.response.send_message("You have to select a default wait time!", ephemeral=True)
+            return
+
+        # Get amount of seconds to wait by taking about of days * amount of seconds in a day.
+        wait_time = int(self.wait_time) * 86,400
 
         guild = self.db.get_guild(interaction.guild_id)
         if not guild:
-            # set some initial values that should very much be changed by the
-            # user later
+            # Set some initial config stuff from the values we just recieved.
             self.db.add_guild(interaction.guild_id, (0, 0, 0),
                               self.mod_category_id, time.time(),
-                              1_000_000_000,)
+                              wait_time,)
             guild = self.db.get_guild(interaction.guild_id)
 
+        # Register all users who have the selected roles as moderators in the database.
         for role in self.roles:
             for member in role.members:
                 if member.id not in [
                         mod.id for mod in self.db.get_all_moderators()]:
                     self.db.register_moderator(member.id, guild.default_quotas)
 
+        # Disable all the now used dropdowns (as well as the button).
         self.confirm.disabled = True
         self.channel_select.disabled = True
         self.role_select.disabled = True
-        # TODO; make this response an embed
+        self.wait_time_select.disabled = True
+
+        # Create embed to update the message with.
+        embed = discord.Embed(title="Config", description="Config set! Please make sure to update your quotas using the ``/set_quotas command!``", color=colour)
+        embed.add_field(name="Moderator category", value=self.mod_category_name, inline=False)
+
         if self.roles:
-            await interaction.channel.send(f"mod category is: {self.mod_category_name}\n you have registered the following roles as admins: {', '.join([r.name for r in self.roles])[:-2]}")
+            # Can't put a \n in an fstring without python 3.12, and so we spin it out here.
+            # The [:-2] at the end is to remove the final \n from the back of the list.
+            names = '\n'.join([role.name for role in self.roles])[:-2]
+            embed.add_field(name="Registered admins", value=f"You have registered the following roles as admins:\n{names}")
         else:
-            await interaction.channel.send(f"mod category is: {self.mod_category_name}\n you have not registered any roles as admins")
-        await interaction.response.edit_message(view=self)
+            embed.add_field(name="Registered admins", value="You have not registered any roles as admin.")
+        
+        # Update the embed in the sent message.
+        await interaction.response.edit_message(view=self, embed=embed)
         self.stop()
 
 
@@ -71,6 +112,8 @@ class ModManager(commands.Cog):
         self.bot = bot
         self.db: DBHandler = bot.db
 
+        # Register context menu commands (right click commands)
+        # and set their callbacks.
         self.ctx_register_moderator = app_commands.ContextMenu(
             name="Register moderator", callback=self.register_moderator)
         self.ctx_deregister_moderator = app_commands.ContextMenu(
@@ -84,7 +127,7 @@ class ModManager(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message) -> None:
-        # make sure message is by moderator and not in moderator chats
+        # Make sure message is by moderator and not in moderator chats.
         guild = self.db.get_guild(msg.guild.id)
         if not guild:
             return
@@ -95,7 +138,7 @@ class ModManager(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message_edit(self, before: discord.Message, after: discord.Message) -> None:
-        # make sure message is by moderator and not in moderator chats
+        # Make sure message is by moderator and not in moderator chats.
         guild = self.db.get_guild(after.guild.id)
         if not guild:
             return
@@ -106,11 +149,21 @@ class ModManager(commands.Cog):
 
     @commands.Cog.listener()
     async def on_audit_log_entry_create(self, entry: discord.AuditLogEntry):
-        # make sure the audit log entry is a message deletion, and that it is
-        # done by a moderator in a non moderator channel
+
+        # TODO; Discord's audit logs are a bit strange. A new entry will only be
+        # TODO; created if there is not already an entry containing the same 2 users,
+        # TODO; and channel within the past 5 minutes. I have ideas for how to fix
+        # TODO; this by setting off a coroutine to check if any more entries have
+        # TODO; been created 5 minutes later, and first then add the action entries.
+        # TODO; for now though, this works.
+
+        # Make sure the audit log entry is a message deletion.
         if entry.action == discord.AuditLogAction.message_delete:
+            # Get some extra data so it's easier to pass as arguments later.
             channel_id = entry.extra.channel.id
             category_id = entry.extra.channel.category_id
+
+            # Make sure deletion is by a moderator and not in a moderator chat.
             guild = self.db.get_guild(entry.guild.id)
             if not guild:
                 return
@@ -121,6 +174,13 @@ class ModManager(commands.Cog):
                         entry.created_at.timestamp()), channel_id)
 
     async def register_moderator(self, interaction: discord.Interaction, user: discord.Member) -> None:
+        """Command to register a user as a moderator with the bot.
+
+        Args:
+            interaction (discord.Interaction): The discord interaction obj that is passed automatically.
+            user (discord.Member): The user who the command should run on, is also passed automatically.
+        """
+        # TODO; Make this an embed 
         if user.id not in [mod.id for mod in self.db.get_all_moderators()]:
             guild = self.db.get_guild(interaction.guild_id)
             self.db.register_moderator(user.id, guild.default_quotas)
@@ -129,6 +189,13 @@ class ModManager(commands.Cog):
             await interaction.response.send_message(f"User {user.display_name} is already in the moderator list", ephemeral=True)
 
     async def deregister_moderator(self, interaction: discord.Interaction, user: discord.Member) -> None:
+        """Command to de-register a user as a moderator with the bot.
+
+        Args:
+            interaction (discord.Interaction): The discord interaction obj that is passed automatically.
+            user (discord.Member): The user who the command should run on, is also passed automatically.
+        """
+        # TODO; Make this an embed
         if user.id in [mod.id for mod in self.db.get_all_moderators()]:
             self.db.de_register_moderator(user.id)
             await interaction.response.send_message(f"Removing user {user.display_name} from the moderator list", ephemeral=True)
@@ -136,6 +203,13 @@ class ModManager(commands.Cog):
             await interaction.response.send_message(f"User {user.display_name} is not in the moderator list", ephemeral=True)
 
     async def get_moderator(self, interaction: discord.Interaction, user: discord.Member) -> None:
+        """Command to get a moderator from the bot's moderator list and sends a small status on the user.
+
+        Args:
+            interaction (discord.Interaction): The discord interaction obj that is passed automatically.
+            user (discord.Member): The user who the command should run on, is also passed automatically.
+        """
+        # TODO; Make this an embed
         if user.id in [mod.id for mod in self.db.get_all_moderators()]:
             sent, edited, deleted = self.db.get_amount_of_actions_by_type(
                 0, int(time.time()), user.id)
@@ -145,10 +219,17 @@ class ModManager(commands.Cog):
 
     @app_commands.command(description="Sends the current moderator list as an embed")
     async def list_moderators(self, interaction: discord.Interaction) -> None:
+        """Slash command to send a list of all moderators and their stats as an embed.
+
+        Args:
+            interaction (discord.Interaction): The discord interaction obj that is passed automatically.
+        """
+        # Create embed object.
         embed = discord.Embed(
             title="Moderator list",
             description="Here are all the moderators and how many messages they've sent:",
             color=discord.Color.from_str("#ffffff"))
+        # Add new field to the embed for every moderator.
         for id in [mod.id for mod in self.db.get_all_moderators()]:
             sent, edited, deleted = self.db.get_amount_of_actions_by_type(
                 0, int(time.time()), id)
@@ -159,11 +240,26 @@ class ModManager(commands.Cog):
 
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(description="Configures the bot")
+    @app_commands.command(description="Configures the bot")    
     async def configure(self, interaction: discord.Interaction) -> None:
-        await interaction.response.send_message("Configure", view=ConfigView(self.db))
+        """Slash command that configures a bot, adds it to the database and makes sure
+        it has all the default settings it requires
 
-    def is_moderator_channel(self, channel: discord.abc.GuildChannel) -> None:
+        Args:
+            interaction (discord.Interaction): The discord interaction obj that is passed automatically.
+        """
+        embed=discord.Embed(title="Config", description="Please fill in the dropdowns at the bottom of this message (and hit the button) to get this bot set up and running on your server. Once you have finished this, please make sure to set a quota for your moderators by running ``/set_quota``!", color=colour)
+        await interaction.response.send_message(view=ConfigView(self.db), embed=embed)
+
+    def is_moderator_channel(self, channel: discord.abc.GuildChannel) -> bool:
+        """Function that checks if the given channel is under the moderator category in it's server.
+
+        Args:
+            channel (discord.abc.GuildChannel): The channel object to check for.
+
+        Returns:
+            bool: If the channel was in the moderator category or not
+        """
         return channel.category.id == self.db.get_guild(
             channel.guild.id,).mod_category_id
 
